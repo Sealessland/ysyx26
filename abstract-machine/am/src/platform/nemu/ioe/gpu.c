c8cdcfe380a2b7126fa0d84ce34bb07a8b72e81d
tracer-ysyx <tracer@ysyx.org>
2026-02-22 21:08:01 +0800

    > compile NEMU ysyx_22040000 张三 Linux sealessland 6.18.12-200.fc43.x86_64 #1 SMP PREEMPT_DYNAMIC Mon Feb 16 18:58:26 UTC 2026 x86_64 GNU/Linux  21:08:01 up 21:41,  2 users,  load average: 1.39, 1.88, 1.44

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -1,28 +1,41 @@
 #include <am.h>
 #include <nemu.h>
-#include<string.h>
+
 #define SYNC_ADDR (VGACTL_ADDR + 4)
+
+#define HEIGHT_MASK 0x0000ffff
+#define WIDTH_MASK 0xffff0000
 
 void __am_gpu_init() {
 }
 
-void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
-  *cfg = (AM_GPU_CONFIG_T) {
-    .present = true, .has_accel = false,
-    .width = 0, .height = 0,
-    .vmemsz = 0
-  };
+void __am_gpu_config(AM_GPU_CONFIG_T *cfg) 
+{
+	cfg->present = true;
+	cfg->has_accel = false;
+	cfg->width = inl(VGACTL_ADDR) >> 16;
+	cfg->height = inl(VGACTL_ADDR) & HEIGHT_MASK;
+	cfg->vmemsz = cfg->width * cfg->height * sizeof(uint32_t);
 }
 
 void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
-  int x = ctl->x, y = ctl->y, w = ctl->w, h = ctl->h;
-  uint32_t *pixels = ctl->pixels;
-  uint32_t *fb = (uint32_t *)FB_ADDR;
-  for (int i = 0; i < h; i++) {
-    uint32_t *dst = fb + (y + i) * 400 + x;
-    uint32_t *src = pixels + i * w;
-    memcpy(dst, src, w * sizeof(uint32_t));
-  } 
+	if (ctl->sync)
+		outl(SYNC_ADDR, 1);
+
+	uint32_t * pixels = (uint32_t *)ctl->pixels;
+	uint32_t pixel;
+	int x = ctl->x, y = ctl->y;
+	int w = ctl->w, h = ctl->h;
+	int width = inl(VGACTL_ADDR) >> 16;
+
+	for(int i = 0; i < h; i++)
+	{
+		for(int j = 0; j < w; j++)
+		{
+			pixel = pixels[w * i + j];
+			outl(FB_ADDR + (y + i) * width * sizeof(uint32_t) + (x + j) * sizeof(uint32_t), pixel);
+		}
+	}
 }
 
 void __am_gpu_status(AM_GPU_STATUS_T *status) {
