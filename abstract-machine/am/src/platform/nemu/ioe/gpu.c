12bd04d43226c0878c25569d87ab844857d10607
tracer-ysyx <tracer@ysyx.org>
2026-02-22 21:07:31 +0800

    > compile NEMU ysyx_22040000 张三 Linux sealessland 6.18.12-200.fc43.x86_64 #1 SMP PREEMPT_DYNAMIC Mon Feb 16 18:58:26 UTC 2026 x86_64 GNU/Linux  21:07:31 up 21:41,  2 users,  load average: 1.02, 1.88, 1.43

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -1,6 +1,6 @@
 #include <am.h>
 #include <nemu.h>
-
+#include<string.h>
 #define SYNC_ADDR (VGACTL_ADDR + 4)
 
 void __am_gpu_init() {
@@ -15,9 +15,14 @@ void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
 }
 
 void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
-  if (ctl->sync) {
-    outl(SYNC_ADDR, 1);
-  }
+  int x = ctl->x, y = ctl->y, w = ctl->w, h = ctl->h;
+  uint32_t *pixels = ctl->pixels;
+  uint32_t *fb = (uint32_t *)FB_ADDR;
+  for (int i = 0; i < h; i++) {
+    uint32_t *dst = fb + (y + i) * 400 + x;
+    uint32_t *src = pixels + i * w;
+    memcpy(dst, src, w * sizeof(uint32_t));
+  } 
 }
 
 void __am_gpu_status(AM_GPU_STATUS_T *status) {
