2948c7d46cf2ca4437723915b31d2876dc1c46f7
tracer-ysyx <tracer@ysyx.org>
2026-02-23 15:25:02 +0800

    > run NEMU ysyx_22040000 张三 Linux sealessland 6.18.12-200.fc43.x86_64 #1 SMP PREEMPT_DYNAMIC Mon Feb 16 18:58:26 UTC 2026 x86_64 GNU/Linux  15:25:02 up 1 day, 15:58,  2 users,  load average: 1.19, 1.05, 1.18

--- abstract-machine/am/src/riscv/nemu/cte.c
+++ abstract-machine/am/src/riscv/nemu/cte.c
@@ -4,44 +4,17 @@
 
 static Context* (*user_handler)(Event, Context*) = NULL;
 
-#define MCAUSE_ECALL_U 8
-#define MCAUSE_ECALL_S 9
-#define MCAUSE_ECALL_M 11
-
-#if __riscv_xlen == 64
-#define MCAUSE_IRQ_TIMER_M 0x8000000000000007ull
-#define MCAUSE_IRQ_TIMER_S 0x8000000000000005ull
-#else
-#define MCAUSE_IRQ_TIMER_M 0x80000007ul
-#define MCAUSE_IRQ_TIMER_S 0x80000005ul
-#endif
-
 Context* __am_irq_handle(Context *c) {
   if (user_handler) {
     Event ev = {0};
     switch (c->mcause) {
-      case MCAUSE_ECALL_U:
-      case MCAUSE_ECALL_S:
-      case MCAUSE_ECALL_M:
-        if (c->GPR1 == -1) { 
-          ev.event = EVENT_YIELD;
-        } else {
-          ev.event = EVENT_SYSCALL;
-        }
-        c->mepc += 4; // 同步异常，必须 +4 跳过 ecall 指令
-        break;
-      case MCAUSE_IRQ_TIMER_S:
-      case MCAUSE_IRQ_TIMER_M:
-        ev.event = EVENT_IRQ_TIMER;
-        // 中断是异步的，返回时应该重新执行被中断的指令，不需要 +4
-        break;
-      default: 
-        ev.event = EVENT_ERROR; 
-        break;
+      default: ev.event = EVENT_ERROR; break;
     }
+
     c = user_handler(ev, c);
     assert(c != NULL);
   }
+
   return c;
 }
 
@@ -58,19 +31,7 @@ bool cte_init(Context*(*handler)(Event, Context*)) {
 }
 
 Context *kcontext(Area kstack, void (*entry)(void *), void *arg) {
-  Context *c = (Context *)((uintptr_t)kstack.end - sizeof(Context));
-  
-  memset(c, 0, sizeof(Context));
-  
-  c->mepc = (uintptr_t)entry;
-  c->mstatus = 0x1800; // M-mode
-  
-  // RISC-V ABI 规定 a0 传参
-  c->gpr[10] = (uintptr_t)arg; 
-  // 设置 sp (x2) 寄存器
-  c->gpr[2] = (uintptr_t)c; 
-  
-  return c;
+  return NULL;
 }
 
 void yield() {
